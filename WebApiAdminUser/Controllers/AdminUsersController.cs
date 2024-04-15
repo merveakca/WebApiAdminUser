27518cc9eb17e8af864b5f49853201f5d3f99ed6
merveakca <smkirac@gmail.com>
2024-04-16 02:41:20 +0300

    .

---
+++ WebApiAdminUser/Controllers/AdminUsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiAdminUser.Models;
+
+namespace WebApiAdminUser.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AdminUsersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        AdminUserDbContext db = new AdminUserDbContext();
+    }
+
+
+    [HttpGet("{id}")]
+    public IActionResult Get()
+    {
+
+    }
+
+
+    [HttpPost]
+    public IActionResult Add()
+    {
+
+    }
+
+
+    [HttpPut]
+    public IActionResult Update()
+    {
+
+    }
+
+
+    [HttpDelete]
+    public IActionResult Delete()
+    {
+
+    }
+
+}
