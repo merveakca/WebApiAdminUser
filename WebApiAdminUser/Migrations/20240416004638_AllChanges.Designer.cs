dca81ae42cddc5b106e9ec73f9e87995bb91e12a
merveakca <smkirac@gmail.com>
2024-04-16 03:49:05 +0300

    .

--- WebApiAdminUser/Migrations/20240416004638_AllChanges.Designer.cs
+++ WebApiAdminUser/Migrations/20240416004638_AllChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApiAdminUser.Models;
 
@@ -11,9 +12,11 @@ using WebApiAdminUser.Models;
 namespace WebApiAdminUser.Migrations
 {
     [DbContext(typeof(AdminUserDbContext))]
-    partial class AdminUserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240416004638_AllChanges")]
+    partial class AllChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebApiAdminUser/Migrations/20240416004638_AllChanges.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiAdminUser.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "AdminUsers",
+                newName: "AdminUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AdminUserId",
+                table: "AdminUsers",
+                newName: "Id");
+        }
+    }
+}
