e21c3bddc148302951744e15fa71694025c8bd4f
merveakca <smkirac@gmail.com>
2024-04-16 03:29:17 +0300

    .

--- WebApiAdminUser/Controllers/AdminUsersController.cs
+++ WebApiAdminUser/Controllers/AdminUsersController.cs
@@ -7,38 +7,75 @@ namespace WebApiAdminUser.Controllers;
 [ApiController]
 public class AdminUsersController : ControllerBase
 {
+    AdminUserDbContext db;
+    public AdminUsersController()
+    {
+        db = new AdminUserDbContext();
+    }
+
+
     [HttpGet]
     public IActionResult GetAll()
     {
-        AdminUserDbContext db = new AdminUserDbContext();
+        List<AdminUser> adminUsers = db.AdminUsers.ToList();
+
+        return Ok(adminUsers);
     }
 
 
     [HttpGet("{id}")]
-    public IActionResult Get()
+    public IActionResult Get(int id)
     {
+        AdminUser adminUser = db.AdminUsers.FirstOrDefault(x => x.AdminUserId == id);
 
+        if (adminUser == null)
+        {
+            return NotFound("Böyle bir kullanıcı bulunmamaktadır.");
+        }
+
+        return Ok(adminUser);
     }
 
 
     [HttpPost]
-    public IActionResult Add()
+    public IActionResult Add(AdminUser adminUser)
     {
+        db.AdminUsers.Add(adminUser);
+        db.SaveChanges();
 
+        return Ok(adminUser);
     }
 
 
     [HttpPut]
-    public IActionResult Update()
+    public IActionResult Update(AdminUser adminUser)
     {
+        // önce güncellenecek data bulunur.
+        AdminUser user = db.AdminUsers.FirstOrDefault(x => x.AdminUserId == adminUser.AdminUserId);
 
+        user.Name = adminUser.Name;
+        user.Surname = adminUser.Surname;
+        user.Email = adminUser.Email;
+        user.Phone = adminUser.Phone;
+        user.IsDeleted = adminUser.IsDeleted;
+
+        db.SaveChanges();
+        return Ok(adminUser);
     }
 
 
-    [HttpDelete]
-    public IActionResult Delete()
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
     {
+        AdminUser adminUser = db.AdminUsers.Find(id);
 
+        if (adminUser == null)
+        {
+            return NotFound("Kullanıcı bulunamadı");
+        }
+
+        db.AdminUsers.Remove(adminUser);
+        db.SaveChanges();
+        return Ok("İşlem başarılı");
     }
-
 }
--- WebApiAdminUser/Models/AdminUser.cs
+++ WebApiAdminUser/Models/AdminUser.cs
@@ -4,7 +4,7 @@ namespace WebApiAdminUser.Models;
 
 public class AdminUser
 {
-    public int Id { get; set; }
+    public int AdminUserId { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
     public string? Email { get; set; }
